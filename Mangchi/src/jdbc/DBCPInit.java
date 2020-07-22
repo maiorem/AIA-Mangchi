59ca0bcd8b93b34f3181aa788d950c5ecfd16b6a
BogyeumKim <namastae112@gmail.com>
2020-07-22 23:35:14 +0900

    깃푸쉬오류해결

--- Mangchi/src/jdbc/DBCPInit.java
+++ Mangchi/src/jdbc/DBCPInit.java
@@ -35,10 +35,13 @@ public class DBCPInit extends HttpServlet {
 		
 		try {
 			
+		
+
 			//String jdbcDriver = "jdbc:mysql://mangchi1.cynhwcnt3hk0.ap-northeast-2.rds.amazonaws.com:3306/project?autoReconnect=true&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 			String jdbcDriver = "jdbc:mysql://localhost:3306/project?autoReconnect=true&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 			String username = "mangchi";
 			String pw = "root";
+
 			
 			ConnectionFactory connFactory = new DriverManagerConnectionFactory(jdbcDriver, username, pw);
 			
