e283c792ed67d987bbe0190b74442819a2cdf3ba
alikwon <63032589+alikwon@users.noreply.github.com>
2020-07-22 18:34:38 +0900

    fd

--- Mangchi/src/board/dao/BoardDao.java
+++ Mangchi/src/board/dao/BoardDao.java
@@ -1,5 +1,39 @@
 package board.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class BoardDao {
- 
+	private BoardDao() {}
+	
+	private static BoardDao dao = new BoardDao();
+	public static BoardDao getInstance() {
+		return dao;
+	}
+	public int getNextIdx(Connection conn) throws SQLException {
+		int result=0;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt=conn.createStatement();
+			String sql = "select ifnull(max(req_idx)+1,1)from project.request_list";
+			rs=stmt.executeQuery(sql);
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+		}finally {
+			if(stmt!=null) {
+				stmt.close();
+			}
+			if(rs!=null) {
+				rs.close();
+			}
+		}
+		
+		return result;
+	}
+	
+	
 }
--- Mangchi/src/board/service/ReqWriteServiceImpl.java
+++ Mangchi/src/board/service/ReqWriteServiceImpl.java
@@ -1,15 +1,56 @@
 package board.service;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import board.dao.BoardDao;
+import board.model.RequestWriting;
+import jdbc.ConnectionProvider;
 import service.Service;
 
 public class ReqWriteServiceImpl implements Service {
-
+	BoardDao dao;
 	@Override
-	public String getViewPage(HttpServletRequest req, HttpServletResponse resp) {
-		return "/WEB-INF/views/index.jsp";
+	public String getViewPage(HttpServletRequest request, HttpServletResponse response) {
+		Connection conn=null;
+		int req_idx;
+		String req_title;	//글제목
+		int req_price;		//금액
+		Date req_term;		//반납일시
+		String req_loc;		//주소
+		String req_text;	//상세내용
+		double req_latitude;//위도
+		double req_longitude;//경도
+		String req_img;		//참고이미지
+		System.out.println(request.getParameter("req_term"));
+		req_idx=Integer.parseInt(request.getParameter("req_idx"));
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		String term_str= request.getParameter("req_term");
+		term_str = term_str.replace("T", " ");
+		try {
+			req_term=df.parse(term_str);
+			System.out.println(req_term);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "/WEB-INF/views/requestWriteForm.jsp";
 	}
 
 }
