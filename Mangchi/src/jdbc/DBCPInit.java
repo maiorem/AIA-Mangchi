406f133512242629bd3fca2ef55f9d66040a52a6
Min <kas4947@gmail.com>
2020-07-23 20:56:46 +0900

    .

--- Mangchi/src/jdbc/DBCPInit.java
+++ Mangchi/src/jdbc/DBCPInit.java
@@ -34,11 +34,6 @@ public class DBCPInit extends HttpServlet {
 	private void initConnectionPool() {
 		
 		try {
-<<<<<<< HEAD
-	
-=======
-
->>>>>>> PARKSEONGMIN
 			String jdbcDriver = "jdbc:mysql://mangchi1.cynhwcnt3hk0.ap-northeast-2.rds.amazonaws.com:3306/project?autoReconnect=true&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 			String username = "bit";
 			String pw = "bit";
