e4dd786696c9b1bdf4b2febb54e5bdce4d49109f
alikwon <63032589+alikwon@users.noreply.github.com>
2020-07-25 15:34:37 +0900

    Update DetailRequestInfoImpl.java

--- Mangchi/src/board/service/DetailRequestInfoImpl.java
+++ Mangchi/src/board/service/DetailRequestInfoImpl.java
@@ -23,9 +23,9 @@ public class DetailRequestInfoImpl implements Service {
 		Member loginMember = (Member)req.getSession().getAttribute("loginInfo");
 		int loginIdx=loginMember.getIdx();
 		
-//		int req_idx = Integer.parseInt(req.getParameter("req_idx"));
+		int req_idx = Integer.parseInt(req.getParameter("req_idx"));
 		
-		int req_idx=77; 
+//		int req_idx=77; 
 		try {
 			conn = ConnectionProvider.getConnection();
 			dao=BoardDao.getInstance();
